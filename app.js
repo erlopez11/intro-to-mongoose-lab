1be2548e0a6b3e08f251ddefc61eb1d9c7c8d6d0
erlopez11 <lopezelainy@gmail.com>
2025-03-05 20:54:07 -0600

    fix bug where 2 new customers added at once, moved prompt texts to inside of runQueries and add conditional on if the action selected was 1

--- app.js
+++ app.js
@@ -4,28 +4,28 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const prompt = require('prompt-sync')();
 
 
 //Customer Model
-const  Customer = require('./models/customer');
+const Customer = require('./models/customer');
 
 const connect = async () => {
-  // Connect to MongoDB using the MONGODB_URI specified in our .env file.
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log('Connected to MongoDB');
+    // Connect to MongoDB using the MONGODB_URI specified in our .env file.
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
 
-  // Call the runQueries function, which will eventually hold functions to work
-  // with data in our db.
-  await runQueries()
+    // Call the runQueries function, which will eventually hold functions to work
+    // with data in our db.
+    await runQueries()
 
-  // Disconnect our app from MongoDB after our queries run.
-  await mongoose.disconnect();
-  console.log('Disconnected from MongoDB');
+    // Disconnect our app from MongoDB after our queries run.
+    await mongoose.disconnect();
+    console.log('Disconnected from MongoDB');
 
-  // Close our app, bringing us back to the command line.
-  process.exit();
+    // Close our app, bringing us back to the command line.
+    process.exit();
 };
 
 //CRUD Methods
@@ -42,34 +42,29 @@ const createCustomer = async () => {
 
     const customer = await Customer.create(customerData);
     console.log('New Customer', customer);
-    
+
 };
 
 
-
-function actionPrompt() {
-
+const runQueries = async () => {
+    console.log('Queries running.')
     let actionsList = [
-        {option: '1. Create a customer'},
-        {option: '2. View all customers'},
-        {option: '3. Update a customer'},
-        {option: '4. Delete a customer'},
-        {option: '5. Quit'},
+        { option: '1. Create a customer' },
+        { option: '2. View all customers' },
+        { option: '3. Update a customer' },
+        { option: '4. Delete a customer' },
+        { option: '5. Quit' },
     ]
-
+    
     console.log('Welcome to the CRM');
     console.log('What would you like to do?');
-    actionsList.forEach(item => {console.log(item.option)});
+    actionsList.forEach(item => { console.log(item.option) });
     const action = prompt('Number of action to run:');
-    
-}
 
-actionPrompt();
-
-const runQueries = async () => {
-  console.log('Queries running.')
-  // The functions calls to run queries in our db will go here as we write them.
-  await createCustomer();
+    // The functions calls to run queries in our db will go here as we write them.
+    if (action === '1') {
+        await createCustomer();
+    }
 };
 
 connect()
